f7f79abb110848fc72da4db6ffff1e5981034dc8
WOLK_ASE <WOLK_ASE@ukr.net>
2019-05-28 15:40:33 +0300

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="container">
+        <div class="f-s-b">
+      <div class="logo">Logo</div>
+      <div class="abz">abz.agency</div>
+      <nav class="f-c">
+        <router-link class="header__link link" to="/">About me</router-link>
+        <router-link class="header__link link" to="/">Relationships</router-link>
+        <router-link class="header__link link" to="/">Requirements</router-link>
+        <router-link class="header__link link" to="/">Users</router-link>
+        <router-link class="header__link link" to="/">Sign Up</router-link>
+      </nav>
+        <div class="user_name">Superstar <br> Superstar@gmail.com</div>
+        <div class="user_img">Img</div>
+        <div class="login_logo">Login</div>
+        </div>
+    </div>
+      <h5>The quick</h5>
+      <h2>The quick</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style>
+
+</style>
