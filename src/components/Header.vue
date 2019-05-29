296b091a4fea9bc3db6ebc5496986eacb5737aff
WOLK_ASE <WOLK_ASE@ukr.net>
2019-05-29 12:33:34 +0300

    init

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,9 @@
   <div class="header">
     <div class="container">
       <div class="f-s-b">
-        <div class="logo">Logo</div>
-        <div class="abz">abz.agency</div>
+        <div class="logo"></div>
         <nav class="f-c">
-          <router-link class="header__link link1" to="/">About me</router-link>
+          <router-link class="header__link link1" to="/about">About me</router-link>
           <router-link class="header__link link1" to="/"
             >Relationships</router-link
           >
@@ -19,10 +18,8 @@
           Superstar <br />
           Superstar@gmail.com
         </div>
-        <div class="user_img">Img</div>
-        <div class="login_logo">Login</div>
-        <button class="btn1">Primary</button>
-        <button class="btn2">Secondary</button>
+        <div class="user_img"></div>
+        <div class="sign-out"></div>
       </div>
     </div>
   </div>
@@ -34,4 +31,36 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.logo {
+  background-image: url("../assets/logo/logo.svg");
+  width: 134px;
+  height: 24px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+  .header__link {
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-top: 24px;
+  margin-bottom: 24px;
+}
+.user_name {
+  text-align: end;
+
+}
+.user_img {
+background-image: url("../assets/imgs/user-superstar-2x.jpg");
+  background-size: cover;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+.sign-out {
+  background-image: url("../assets/icons/sign-out.svg");
+  width: 24px;
+  height: 20px;
+  margin-top: 22px;
+
+}
+</style>
