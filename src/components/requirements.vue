3c09e0c3b09a8a264b3ae253606ac90c46d31c73
WOLK_ASE <WOLK_ASE@ukr.net>
2019-05-31 14:47:25 +0300

    init

--- src/components/requirements.vue
+++ src/components/requirements.vue
@@ -10,13 +10,13 @@
           that people really care about how quickly their pages load.<br/> The Search
           team announced speed would be a ranking signal for<br/> desktop searches in
           2010 and as of this month(July 2018), page<br/> speed will be a ranking
-          factor for mobile searches too.<br/><br/> If you're a developer working on site,
+          factor for mobile searches too.<br/><br/> <br/>If you're a developer working on site,
           now is a good time to<br/> evaluate your performance using our speed tools.
           Think about how<br/> performance affects the user experience of your pages
           and consider<br/> measuring a variety of real-world user-centric
           performance metrics.<br/><br/> Are you shipping to much JavaScript? Too many
           images? Images<br/> and JavaScript are most significant contributors to the
-          page weight that affect page<br/> load time based on data from HTTP Archive<br/>
+          page<br/> weight that affect page load time based on data from HTTP Archive<br/>
           and the Chrome User Experience Report - our public dataset for key<br/> UX
           metrics as experienced by Chrome user under real-world<br/> conditions.
         </p>
