a7e066f91176510efa3c9ef6e089eaed6d24a216
WOLK_ASE <WOLK_ASE@ukr.net>
2019-06-02 09:28:00 +0300

    init

--- src/components/requirements.vue
+++ src/components/requirements.vue
@@ -4,22 +4,25 @@
       <div class="h2 requirements-title">
         General requirements for the test task
       </div>
-      <div class="f-j-c">
-        <p class="medium">
-          Users want to find answers to their questions quickly and data<br/> shows
-          that people really care about how quickly their pages load.<br/> The Search
-          team announced speed would be a ranking signal for<br/> desktop searches in
-          2010 and as of this month(July 2018), page<br/> speed will be a ranking
-          factor for mobile searches too.<br/><br/> <br/>If you're a developer working on site,
-          now is a good time to<br/> evaluate your performance using our speed tools.
-          Think about how<br/> performance affects the user experience of your pages
-          and consider<br/> measuring a variety of real-world user-centric
-          performance metrics.<br/><br/> Are you shipping to much JavaScript? Too many
-          images? Images<br/> and JavaScript are most significant contributors to the
-          page<br/> weight that affect page load time based on data from HTTP Archive<br/>
-          and the Chrome User Experience Report - our public dataset for key<br/> UX
-          metrics as experienced by Chrome user under real-world<br/> conditions.
+      <div class="f-j-c requirements-data">
+        <p class="medium requirements-data__text">
+          Users want to find answers to their questions quickly and data<br />
+          shows that people really care about how quickly their pages load.<br />
+          The Search team announced speed would be a ranking signal for<br />
+          desktop searches in 2010 and as of this month(July 2018), page<br />
+          speed will be a ranking factor for mobile searches too.<br />
+          <br />If you're a developer working on site, now is a good time to<br />
+          evaluate your performance using our speed tools. Think about how<br />
+          performance affects the user experience of your pages and consider<br />
+          measuring a variety of real-world user-centric performance metrics.<br /><br />
+          Are you shipping to much JavaScript? Too many images? Images<br />
+          and JavaScript are most significant contributors to the page<br />
+          weight that affect page load time based on data from HTTP Archive<br />
+          and the Chrome User Experience Report - our public dataset for key<br />
+          UX metrics as experienced by Chrome user under real-world<br />
+          conditions.
         </p>
+        <div class="requirements-data__img"></div>
       </div>
     </div>
   </div>
@@ -31,4 +34,29 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+.requirements {
+  background-image: url("../assets/imgs/bg-2.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.requirements-title {
+  color: white;
+  text-align: center;
+  padding: 130px 0 25px 0;
+}
+.requirements-data {
+  padding-bottom: 139px;
+}
+.requirements-data__text {
+  color: #fefefe;
+  line-height: 22px;
+}
+.requirements-data__img {
+  background-image: url("../assets/imgs/man-laptop-v1.svg");
+  width: 429px;
+  height: 361px;
+  margin-left: 53px;
+  margin-top: 29px;
+}
+</style>
